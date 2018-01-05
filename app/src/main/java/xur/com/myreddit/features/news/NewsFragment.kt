c5fee33f695dea665309f6e00bbf8e9e3e884799
xur <xur@2345.com>
2018-01-05 15:36:17 +0800

    newsList lazy

--- app/src/main/java/xur/com/myreddit/features/news/NewsFragment.kt
+++ app/src/main/java/xur/com/myreddit/features/news/NewsFragment.kt
@@ -3,10 +3,10 @@ package xur.com.myreddit.features.news
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.support.v7.widget.LinearLayoutManager
-import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import kotlinx.android.synthetic.main.news_fragment.*
 import xur.com.myreddit.R
 import xur.com.myreddit.common.inflate
 
@@ -14,14 +14,24 @@ import xur.com.myreddit.common.inflate
  * Created by xur on 2018/1/4.
  */
 class NewsFragment : Fragment() {
-    private var newsList: RecyclerView? = null
+    //private var newsList: RecyclerView? = null
+    private val newsList by lazy {
+        news_list
+    }
 
     override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val view = container?.inflate(R.layout.news_fragment)
-        newsList = view?.findViewById<RecyclerView>(R.id.news_list) as RecyclerView
+/*        newsList = view?.findViewById<RecyclerView>(R.id.news_list) as RecyclerView
         newsList?.setHasFixedSize(true)
-        newsList?.layoutManager = LinearLayoutManager(context)
-        return view
+        newsList?.layoutManager = LinearLayoutManager(context)*/
+
+        return container?.inflate(R.layout.news_fragment)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        newsList.setHasFixedSize(true)
+        newsList.layoutManager = LinearLayoutManager(context)
     }
 
 }
