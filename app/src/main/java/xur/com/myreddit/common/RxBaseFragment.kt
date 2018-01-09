c41a4c0c06d007d9f7c20922280513d32ce0f71d
xur <xur@2345.com>
2018-01-09 13:06:56 +0800

    add real news

--- app/src/main/java/xur/com/myreddit/common/RxBaseFragment.kt
+++ app/src/main/java/xur/com/myreddit/common/RxBaseFragment.kt
@@ -1,6 +1,6 @@
 package xur.com.myreddit.common
 
-import android.app.Fragment
+import android.support.v4.app.Fragment
 import rx.subscriptions.CompositeSubscription
 
 /**
--- app/src/main/java/xur/com/myreddit/features/news/NewsManager.kt
+++ app/src/main/java/xur/com/myreddit/features/news/NewsManager.kt
@@ -1,29 +1,32 @@
 package xur.com.myreddit.features.news
 
 import rx.Observable
+import xur.com.myreddit.api.RestAPI
 import xur.com.myreddit.common.RedditNewsItem
 
 
 /**
  * Created by xur on 2018/1/7.
  */
-class NewsManager() {
-    fun getNews(): Observable<List<RedditNewsItem>> {
+class NewsManager(private val api: RestAPI = RestAPI()) {
+    fun getNews(limit: String = "10"): Observable<List<RedditNewsItem>> {
         return Observable.create {
             subscriber ->
 
-            val news = mutableListOf<RedditNewsItem>()
-            for (i in 1..10) {
-                news.add(RedditNewsItem(
-                        "author$i",
-                        "Title $i",
-                        i, // number of comments
-                        1457207701L - i * 200, // time
-                        "http://lorempixel.com/200/200/technics/$i", // image url
-                        "url"
-                ))
+            val callResponse = api.getNews("", limit)
+            val response = callResponse.execute()
+
+            if (response.isSuccessful) {
+                val news = response.body().data.children.map {
+                    val item = it.data
+                    RedditNewsItem(item.author, item.title, item.num_comments,
+                            item.created, item.thumbnail, item.url)
+                }
+                subscriber.onNext(news)
+                subscriber.onCompleted()
+            } else {
+                subscriber.onError(Throwable(response.message()))
             }
-            subscriber.onNext(news)
         }
     }
 }
